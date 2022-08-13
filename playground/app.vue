9a75ba216a54460fc14e3b79b8de62761c56bbcb
Pooya Parsa <pooya@pi0.io>
2022-08-13 13:08:40 +0200

    chore: add footer to playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
+  <div class="m-4 text-center">
     <h2 class="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-300">
       Hey there!
     </h2>
+    <hr>
+    Powered by <NuxtLink to="https://v3.nuxtjs.org" class="underline">
+      Nuxt 3
+    </NuxtLink>,  <NuxtLink to="https://twind.style/" class="underline">
+      Twind
+    </NuxtLink> and <NuxtLink to="https://github.com/pi0/nuxt-twind" class="underline">
+      Nuxt-Tailwind
+    </NuxtLink>
   </div>
 </template>
 
